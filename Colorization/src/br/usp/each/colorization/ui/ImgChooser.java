7454c74611c1511f67aab58344dcccc5df8bbe26
Leonardo Rocha <leonardo.lsrocha@gmail.com>
2015-11-06 15:08:31 -0200

    Implementing file chooser [non-tested]

--- Colorization/src/br/usp/each/colorization/ui/ImgChooser.java
+++ Colorization/src/br/usp/each/colorization/ui/ImgChooser.java
@@ -1,9 +1,45 @@
 package br.usp.each.colorization.ui;
 
-public class ImgChooser {
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-	public ImgChooser() {
-		// TODO Auto-generated constructor stub
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+public class ImgChooser implements ActionListener {
+
+	private JFileChooser chooser;
+
+	public ImgChooser(JPanel canvas) {
+		this.chooser = new JFileChooser();
+		this.chooser.setMultiSelectionEnabled(false);
 	}
 
+	public JButton getButton(String label) {
+		JButton button = new JButton(label);
+		button.addActionListener(this);
+
+		return button;
+	}
+
+	public String selectFile(Component parent) {
+		int returnVal = this.chooser.showOpenDialog(parent);
+		if (returnVal != JFileChooser.APPROVE_OPTION) return null;
+
+		File selectedFile = this.chooser.getSelectedFile();
+		return selectedFile.getPath();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			JButton button = (JButton) e.getSource();
+			System.out.println(this.selectFile(button.getParent()));
+		} catch (ClassCastException ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
 }
