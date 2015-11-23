02036c8b7028010f0e35c47b2edc1ef58959a9be
Tiago <Tiago@Nelson-PC>
2015-11-23 01:48:48 -0200

    Dye

--- Colorization/src/br/usp/each/colorization/processing/ColorPropagation.java
+++ Colorization/src/br/usp/each/colorization/processing/ColorPropagation.java
@@ -2,6 +2,7 @@ package br.usp.each.colorization.processing;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -24,15 +25,24 @@ public class ColorPropagation {
 		// TODO Image processing following the algorithm
 		ArrayList<Line> l = canvas.getPaintedLines();
 		Line j;
-		Line2D p;
-        BufferedImage image=new BufferedImage(canvas.getWidth(), canvas.getHeight(),BufferedImage.TYPE_INT_RGB);
+
+		Line2D p = null;
+        BufferedImage image= canvas.getImage();
+
+
 		Graphics2D g2=(Graphics2D)image.getGraphics();
 		PathIterator pit = p.getPathIterator(null);
+		
 		for(int i=0;i<l.size();i++){
 			j=l.get(i);
 			p=j.getLine2D();
+
+			
+			dye(image,j.color);
+
 			g2.drawLine(j.p1.x,j.p1.y,j.p2.x,j.p2.y);
 			pit.next();
+
 		}
 		for(int i=0;i<l.size();i++){
 			j=l.get(i);
@@ -43,5 +53,17 @@ public class ColorPropagation {
 				
 			}
 		
+	private static void dye(BufferedImage image, Color color)
+    {
+	 int w = image.getWidth();
+        int h = image.getHeight();
+        BufferedImage dyed = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = dyed.createGraphics();
+        g.drawImage(image, 0,0, null);
+        g.setComposite(AlphaComposite.SrcAtop);
+        g.setColor(color);
+        g.fillRect(0,0,w,h);
+        g.dispose();
+        return;
+    }
 	}
-
