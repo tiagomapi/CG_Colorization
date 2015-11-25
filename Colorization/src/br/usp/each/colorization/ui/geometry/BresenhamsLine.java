d492dc606471bcd116199093ad2ad656d8d0083d
Leonardo Rocha <leonardo.lsrocha@gmail.com>
2015-11-25 14:40:45 -0200

    Creating Bresenham's Line Algorithm class

---
+++ Colorization/src/br/usp/each/colorization/ui/geometry/BresenhamsLine.java
@@ -0,0 +1,12 @@
+package br.usp.each.colorization.ui.geometry;
+
+import java.awt.Color;
+
+public class BresenhamsLine extends Line {
+
+	public BresenhamsLine(Point p1, Point p2, Color color) {
+		super(p1, p2, color);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
